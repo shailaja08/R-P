fe9a147fdf90b3fa06f03a37c821b2edf99e2d44
Shailaja Yadav <Shailaja Yadav>
2017-03-02 00:24:57 +0530

    Test

---
+++ Apply functions.R
@@ -0,0 +1,6 @@
+###Apply Function in R
+##Apply function helps to apply function to matrix row or column and returns a vector,list or list
+##Type-1 -->Lapply( function applied on list)
+list <- list(a=c(1,1),b=c(2,2),c=c(3,3))
+list
+lapply(list,sum)
