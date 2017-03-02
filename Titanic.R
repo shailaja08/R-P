bbfc33f9226f409b6b00e079eeda916cbda7d9d7
shailaja08 <shailaja.yadav107@gmail.com>
2017-03-02 18:32:27 +0530

    Update Titanic.R

--- Titanic.R
+++ Titanic.R
@@ -34,7 +34,29 @@ class(trainSet$Survived)
 trainSet$Survived<-as.factor(trainSet$Survived)
 
 set.seed(42)
-?set.seed
+model <- train(Survived ~ Pclass + Sex + SibSp + Embarked + Parch + Fare, # Survived is a function of the variables we decided to include
+               data = trainSet, # Use the trainSet dataframe as the training data
+               method = "rf",# Use the "random forest" algorithm
+               trControl = trainControl(method = "cv", # Use cross-validation
+                                        number = 5) # Use 5 folds for cross-validation
+)
+
+model
+library(caret)
+testSet$Survived <- predict(model, newdata = testSet)
+summary(testSet)
+
+## ------------------------------------------------------------------------
+testSet$Fare <- ifelse(is.na(testSet$Fare), mean(testSet$Fare, na.rm = TRUE), testSet$Fare)
+
+## ------------------------------------------------------------------------
+testSet$Survived <- predict(model, newdata = testSet)
+
+## ------------------------------------------------------------------------
+submission <- testSet[,c("PassengerId", "Survived")]
+submission
+write.table(submission, file = "submission.csv", col.names = TRUE, row.names = FALSE, sep = ",")
+
 
 
 
